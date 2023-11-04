dd2a7c239db8bb6d0d0aef303d9c43c1bf4d80d5
Steven King <steveking16@outlook.com>
2023-11-04 11:16:27 -0700

    format and print

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -12,93 +12,91 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
-
 public class SwerveSubsystem extends SubsystemBase {
   private final SwerveModule frontLeft = new SwerveModule(
-    DriveConstants.kFrontLeftDriveMotorPort,
-    DriveConstants.kFrontLeftTurningMotorPort,
-    DriveConstants.kFrontLeftDriveEncoderReversed,
-    DriveConstants.kFrontLeftTurningEncoderReversed,
-    DriveConstants.kFrontLeftDriveAbsoluteEncoderOffsetRad,
-    DriveConstants.kFrontLeftDriveAbsoluteEncoderReversed);
+      DriveConstants.kFrontLeftDriveMotorPort,
+      DriveConstants.kFrontLeftTurningMotorPort,
+      DriveConstants.kFrontLeftDriveEncoderReversed,
+      DriveConstants.kFrontLeftTurningEncoderReversed,
+      DriveConstants.kFrontLeftDriveAbsoluteEncoderOffsetRad,
+      DriveConstants.kFrontLeftDriveAbsoluteEncoderReversed);
 
-private final SwerveModule frontRight = new SwerveModule(
-    DriveConstants.kFrontRightDriveMotorPort,
-    DriveConstants.kFrontRightTurningMotorPort,
-    DriveConstants.kFrontRightDriveEncoderReversed,
-    DriveConstants.kFrontRightTurningEncoderReversed,
-    DriveConstants.kFrontRightDriveAbsoluteEncoderOffsetRad,
-    DriveConstants.kFrontRightDriveAbsoluteEncoderReversed);
+  private final SwerveModule frontRight = new SwerveModule(
+      DriveConstants.kFrontRightDriveMotorPort,
+      DriveConstants.kFrontRightTurningMotorPort,
+      DriveConstants.kFrontRightDriveEncoderReversed,
+      DriveConstants.kFrontRightTurningEncoderReversed,
+      DriveConstants.kFrontRightDriveAbsoluteEncoderOffsetRad,
+      DriveConstants.kFrontRightDriveAbsoluteEncoderReversed);
 
-private final SwerveModule backLeft = new SwerveModule(
-    DriveConstants.kBackLeftDriveMotorPort,
-    DriveConstants.kBackLeftTurningMotorPort,
-    DriveConstants.kBackLeftDriveEncoderReversed,
-    DriveConstants.kBackLeftTurningEncoderReversed,
-    DriveConstants.kBackLeftDriveAbsoluteEncoderOffsetRad,
-    DriveConstants.kBackLeftDriveAbsoluteEncoderReversed);
+  private final SwerveModule backLeft = new SwerveModule(
+      DriveConstants.kBackLeftDriveMotorPort,
+      DriveConstants.kBackLeftTurningMotorPort,
+      DriveConstants.kBackLeftDriveEncoderReversed,
+      DriveConstants.kBackLeftTurningEncoderReversed,
+      DriveConstants.kBackLeftDriveAbsoluteEncoderOffsetRad,
+      DriveConstants.kBackLeftDriveAbsoluteEncoderReversed);
 
-private final SwerveModule backRight = new SwerveModule(
-    DriveConstants.kBackRightDriveMotorPort,
-    DriveConstants.kBackRightTurningMotorPort,
-    DriveConstants.kBackRightDriveEncoderReversed,
-    DriveConstants.kBackRightTurningEncoderReversed,
-    DriveConstants.kBackRightDriveAbsoluteEncoderOffsetRad,
-    DriveConstants.kBackRightDriveAbsoluteEncoderReversed);
+  private final SwerveModule backRight = new SwerveModule(
+      DriveConstants.kBackRightDriveMotorPort,
+      DriveConstants.kBackRightTurningMotorPort,
+      DriveConstants.kBackRightDriveEncoderReversed,
+      DriveConstants.kBackRightTurningEncoderReversed,
+      DriveConstants.kBackRightDriveAbsoluteEncoderOffsetRad,
+      DriveConstants.kBackRightDriveAbsoluteEncoderReversed);
 
+  private final AHRS gyro = new AHRS(SPI.Port.kMXP);
+  private final SwerveDriveOdometry odometer = new SwerveDriveOdometry(DriveConstants.kDriveKinematics,
+      new Rotation2d(), getModulePositions());
 
-    private final AHRS gyro = new AHRS(SPI.Port.kMXP);
-    private final SwerveDriveOdometry odometer = new SwerveDriveOdometry(DriveConstants.kDriveKinematics,
-            new Rotation2d(), getModulePositions());
-
-            public SwerveModulePosition[] getModulePositions() {
-                return new SwerveModulePosition[] {
-                        frontLeft.getPosition(),
-                        frontRight.getPosition(),
-                        backLeft.getPosition(),
-                        backRight.getPosition(),
-                };
-            }
-
-    public SwerveSubsystem() {
-        new Thread(() -> {
-            try {
-                Thread.sleep(1000);
-                zeroHeading();
-            } catch (Exception e) {
-            }
-        }).start();
-    }
-
-    public void zeroHeading() {
-        gyro.reset();
-    }
-
-    public double getHeading() {
-        return Math.IEEEremainder(gyro.getAngle(), 360);
-    }
-
-    public Rotation2d getRotation2d() {
-        return Rotation2d.fromDegrees(getHeading());
-    }
-
-    public void periodic() {
-        SmartDashboard.putNumber("Robot Heading", getHeading());
-    }
-
-    public void stopModules() {
-        frontLeft.stop();
-        frontRight.stop();
-        backLeft.stop();
-        backRight.stop();
-    }
-
-    public void setModuleStates(SwerveModuleState[] desiredStates) {
-        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
-        frontLeft.setDesiredState(desiredStates[0]);
-        frontRight.setDesiredState(desiredStates[1]);
-        backLeft.setDesiredState(desiredStates[2]);
-        backRight.setDesiredState(desiredStates[3]);
-    }
-
+  public SwerveModulePosition[] getModulePositions() {
+    return new SwerveModulePosition[] {
+        frontLeft.getPosition(),
+        frontRight.getPosition(),
+        backLeft.getPosition(),
+        backRight.getPosition(),
+    };
   }
+
+  public SwerveSubsystem() {
+    new Thread(() -> {
+      try {
+        Thread.sleep(1000);
+        zeroHeading();
+      } catch (Exception e) {
+      }
+    }).start();
+  }
+
+  public void zeroHeading() {
+    gyro.reset();
+  }
+
+  public double getHeading() {
+    return Math.IEEEremainder(gyro.getAngle(), 360);
+  }
+
+  public Rotation2d getRotation2d() {
+    return Rotation2d.fromDegrees(getHeading());
+  }
+
+  public void periodic() {
+    SmartDashboard.putNumber("Robot Heading", getHeading());
+  }
+
+  public void stopModules() {
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();
+  }
+
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+    frontLeft.setDesiredState(desiredStates[0]);
+    frontRight.setDesiredState(desiredStates[1]);
+    backLeft.setDesiredState(desiredStates[2]);
+    backRight.setDesiredState(desiredStates[3]);
+  }
+
+}
